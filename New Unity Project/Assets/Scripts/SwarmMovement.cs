3b4e0fbb033d3677a7b89eebe574f848f5106217
Darcy Sandall <d.sandall@hotmail.com>
2016-03-06 00:28:18 +1100

    Added swarms that follow the player

--- New Unity Project/Assets/Scripts/SwarmMovement.cs
+++ New Unity Project/Assets/Scripts/SwarmMovement.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Movement))]
 public class SwarmMovement : MonoBehaviour
 {
     public GameObject _player;
+    public float _acceleration = 40;
+    public float _deceleration = 9;
+    public float _maxSpeed = 10;
+
+    private Movement _movement;
 
 	void Start ()
-    {
-	
+	{
+	    _movement = GetComponent<Movement>();
 	}
 	
 	void Update ()
     {
-	
-	}
+        _movement.MoveTo(_player.transform.position, _acceleration);
+    }
+
+    void FixedUpdate()
+    {
+        _movement.ManageSpeed(_deceleration, _maxSpeed);
+    }
 }
