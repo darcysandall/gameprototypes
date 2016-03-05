fb6087114fa9069865e9eeba3ec428d0508dfe6e
Darcy Sandall <d.sandall@hotmail.com>
2016-03-05 23:51:28 +1100

    Started the swarm prototype

---
+++ New Unity Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform _target;
+    public Vector3 _targetOffset = new Vector3(0, 3.5f, 7);
+    public float _rotateDamping = 100f;
+
+	void Start ()
+    {
+	    if (!_target)
+	    {
+	        Debug.Log("No target is attached to the camera.");
+	    }
+	}
+	
+	void Update ()
+	{
+	    if (!_target) return;
+
+        Follow();
+	    if (_rotateDamping > 0)
+	    {
+	        LookAtTarget();
+	    }
+	    else
+	    {
+	        transform.LookAt(_target.position);
+	    }
+	}
+
+    private void LookAtTarget()
+    {
+        var rotation = Quaternion.LookRotation(_target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotateDamping * Time.deltaTime);
+    }
+
+    private void Follow()
+    {
+        var cameraPosition = _target.position;
+        cameraPosition += _targetOffset;
+        var nextFramePosition = Vector3.Lerp(transform.position, cameraPosition, Time.deltaTime);
+        transform.position = nextFramePosition;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Movement))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float _acceleration = 10f;
+    public float _deceleration = 2f;
+    public float _maxSpeed = 8f;
+
+    private Quaternion _screenSpace;
+    private Vector3 _direction, _moveDirection, _screenForward, _screenRight;
+    private Movement _movement;
+    private Transform _mainCamera;
+
+    void Start()
+    {
+        _movement = GetComponent<Movement>();
+        _mainCamera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        _screenSpace = Quaternion.Euler(0, _mainCamera.eulerAngles.y, 0);
+        _screenForward = _screenSpace * Vector3.forward;
+        _screenRight = _screenSpace * Vector3.right;
+
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        _direction = (_screenForward * vertical) + (_screenRight * horizontal);
+        _moveDirection = transform.position + _direction;
+    }
+
+    void FixedUpdate()
+    {
+        _movement.MoveTo(_moveDirection, _acceleration);
+        _movement.ManageSpeed(_deceleration, _maxSpeed);
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (_direction.magnitude == 0 && _movement.Rigidbody.velocity.magnitude < 2)
+        {
+            _movement.Rigidbody.velocity = Vector3.zero;
+        }
+    }
+}
