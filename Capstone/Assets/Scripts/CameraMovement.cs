17e6706ddcef320c766176c4bce07386b2de3b1e
Ben <wowmaniac2005@hotmail.com>
2016-03-18 02:56:46 +1100

    Created split screen and 4 player cameras with basic movement

---
+++ Capstone/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Class  CameraMovement.
+/// 
+/// Attached to each players camera
+/// </summary>
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform _player;
+    public Vector3 _cameraOffset = new Vector3(0, 6, -6);
+    public Quaternion _cameraRotation = Quaternion.Euler(45f, 0f, 0f);
+
+
+    /// <summary>
+    /// Sets initial camera location and rotation. Called every frame referencing player position and adjusting camera by offset
+    /// </summary>
+    void Update()
+    {
+        if (!_player)
+        {
+            return;
+        }
+        else
+        {
+            transform.position = _player.position + _cameraOffset;
+            transform.rotation = _cameraRotation;
+        }
+
+    }
+}
